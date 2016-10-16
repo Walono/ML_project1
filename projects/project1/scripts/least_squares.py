047942528e7784e72bd58f9f73f8e360b085bbe7
Julie Djeffal <julie.djeffal@gmail.com>
2016-10-16 13:11:10 +0200

    Add Least Squares

---
+++ projects/project1/scripts/least_squares.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+from costs import * 
+
+
+def least_squares(y, tx):
+    w = np.dot(np.dot(np.linalg.inv(np.dot(np.transpose(tx),tx)),np.transpose(tx)),y)
+    mse = compute_loss(y, tx, w)
+    return mse, w
