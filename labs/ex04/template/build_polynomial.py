9f86814f9f96f9642b963b4ed0f911eee5257d3b
Loïs Huguenin <huguenindl@gmail.com>
2016-10-17 12:30:34 +0200

    Bias variance decomposition - Lab4 done

--- labs/ex04/template/build_polynomial.py
+++ labs/ex04/template/build_polynomial.py
@@ -3,12 +3,12 @@
 
 import numpy as np
 
-"""def build_poly(x, degree):
+def build_poly(x, degree):
     polynomial basis function.
     new = np.array([ [ e**i for e in x ] for i in range(degree+1) ])
-    return new.T"""
-def build_poly(x, degree):
-    """polynomial basis functions for input data x, for j=0 up to j=degree.  """
+    return new.T
+"""def build_poly(x, degree):
+    polynomial basis functions for input data x, for j=0 up to j=degree.  
     Phi_tilde = np.ones((len(x),1))
     for i in range(1, degree+1):
         power_column = []
@@ -19,4 +19,4 @@ def build_poly(x, degree):
         #np.concatenate((Phi, power_column), axis=1)
         Phi_tilde = np.concatenate((Phi_tilde,power_column.reshape([len(x),1])), axis=1)
 
-    return Phi_tilde
+    return Phi_tilde"""
